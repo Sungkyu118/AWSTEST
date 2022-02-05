0217e5bf68c822bab71b9105a83f9fac0f918d51
kskcjswo <kskcjswo@naver.com>
2022-02-05 19:07:18 +0900

    test 수정

--- src/test/java/com/example/domain/PostsRepositoryTest.java
+++ src/test/java/com/example/domain/PostsRepositoryTest.java
@@ -54,7 +54,7 @@ public class PostsRepositoryTest {
 
         //then
         Posts posts = postsList.get(0);
-        assertTrue(posts.getCreatedDate().isAfter(now));
-        assertTrue(posts.getModifiedDate().isAfter(now));
+        assertTrue(!posts.getCreatedDate().isAfter(now));
+        assertTrue(!posts.getModifiedDate().isAfter(now));
     }
 }
